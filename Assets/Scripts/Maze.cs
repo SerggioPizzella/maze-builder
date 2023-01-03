5483b810a6dca802922b3939b4fe271d2ac0f036
Pizzella Ricci, Serggio V <serggiopizzella@gmail.com>
2023-01-03 20:51:02 +0100

    Add UI to regenerate the maze

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
@@ -18,12 +20,27 @@ namespace Assets.Scripts
 
         private void Start()
         {
+            GenerateMaze();
+        }
+
+        public void GenerateMaze()
+        {
             generator = new MazeGenerator(width, height);
             visualizer = new MazeVisualizer(width, height);
 
             List<(Position, Position)> transitions = generator.GenerateMaze();
             Mesh mesh = visualizer.GenerateMesh(transitions);
             meshFilter.mesh = mesh;
+        }
+
+        public void SetWidthFromSlider(Slider widthSlider)
+        {
+            this.width = (int)widthSlider.value;
+        }
+
+        public void SetHeightFromSlider(Slider heightSlider)
+        {
+            this.height = (int)heightSlider.value;
         }
     }
 }
